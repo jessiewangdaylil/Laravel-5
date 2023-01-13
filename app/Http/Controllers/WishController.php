7eb95838bc26244724ec84e238f2638d70c7a449
Jessie Wang <jessie.wang.daylil@gmail.com>
2023-01-13 13:51:19 +0800

    init

---
+++ app/Http/Controllers/WishController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class WishController extends Controller
+{
+    public function wish()
+    {
+        return view('wish');
+    }
+}
---
+++ resources/views/wish.blade.php
@@ -0,0 +1,8 @@
+{{-- 繼承 --}}
+@extends('layouts.master')
+{{-- 標籤頁名稱 --}}
+@section('title','首頁')
+
+@section('content')
+
+@endsection

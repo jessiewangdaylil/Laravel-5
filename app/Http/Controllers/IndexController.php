798b59bffb5ecc3b27d22b98e365ccf27830d502
Jessie Wang <jessie.wang.daylil@gmail.com>
2023-01-17 10:44:36 +0800

    結合清瑞、琬渝圖檔+清瑞的model、view

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Element;
+
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $top_titles = Element::where('page', 'index')->where('position', 'top_title')->orderby('sort', 'asc')->get();
+        $top_tips = Element::where('page', 'index')->where('position', 'top_tips')->orderby('sort', 'asc')->get();
+        $top_tabs = Element::where('page', 'index')->where('position', 'top_tab')->orderby('sort', 'asc')->get();
+        $top_classes = Element::where('page', 'index')->where('position', 'class')->orderby('sort', 'asc')->get();
+
+// return $elements;
+        return view('index', compact('top_titles', 'top_tips', 'top_tabs', 'top_classes'));
+
     }
 }
